ddafc580f52c54770226df2d4145f88b82809d25
Wreker <ananyladha28@gmail.com>
2024-07-17 18:21:13 +0530

    Reverse a number

--- src/BasicMaths/ReverseANumber.java
+++ src/BasicMaths/ReverseANumber.java
@@ -1,14 +1,12 @@
 package BasicMaths;
 
-import java.util.Scanner;
-
 public class ReverseANumber {
 
 //    Given a signed 32-bit integer x, return x with its digits reversed.
 //    If reversing x causes the value to go outside the signed 32-bit
 //    integer range [-2^31, 2^31 - 1], then return 0.
 
-    private static int reverse(int n){
+    private int reverse(int n){
         int num = Math.abs(n);
         int rev = 0;
 
