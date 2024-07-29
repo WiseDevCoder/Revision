e33adf431dbe7905aeeae6b5fe5512a26ba108e8
wreker07 <ananyladha28@gmail.com>
2024-07-29 19:01:05 +0530

    Arrays

--- src/Arrays/Easy/RotateArray.java
+++ src/Arrays/Easy/RotateArray.java
@@ -2,9 +2,29 @@ package Arrays.Easy;
 
 public class RotateArray {
 
-    public static void function(int []arr){
+    public static void function(int []arr, int k){
 
+        k %= arr.length;
 
+        if(arr.length == 1){
+            return;
+        }
+
+        int end = arr.length - k; // 4
+
+        rotate(arr, 0, end - 1); // 3
+        rotate(arr, end, arr.length - 1);
+        rotate(arr, 0, arr.length - 1);
+
+    }
+
+    private static void rotate(int []arr, int si, int ei){
+
+        while(si < ei){
+            int temp = arr[si];
+            arr[si++] = arr[ei];
+            arr[ei--] = temp;
+        }
 
     }
 
